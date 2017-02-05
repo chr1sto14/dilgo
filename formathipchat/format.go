cdbf8db9f44984523db970e0dfd63e5f22955473
Christopher <cmiller@selectlabsoft.com>
2017-02-04 22:41:29 -0600

    fixed parsing

--- formathipchat/format.go
+++ formathipchat/format.go
@@ -1,5 +1,84 @@
 package formathipchat
 
+import (
+	"errors"
+	"fmt"
+	"golang.org/x/net/html"
+	"io"
+)
+
 type Message struct {
-	notify bool
+	Color          string `json:"color"`
+	Message        string `json:"message"`
+	Message_format string `json:"message_format"`
+	Notify         bool   `json:"notify"`
+}
+
+func checkTagAttr(t html.Token, tag string, compare string) bool {
+	for _, attr := range t.Attr {
+		if attr.Key == tag {
+			return attr.Val == compare
+		}
+	}
+	return false
+}
+
+func getTagAttr(t html.Token, tag string) (val string, err error) {
+	for _, attr := range t.Attr {
+		if attr.Key == tag {
+			val = attr.Val
+			return
+		}
+	}
+	err = errors.New(fmt.Sprintf("failed to parse %s", tag))
+	return
+}
+
+func parseHtml(body io.Reader) (title string, src string, err error) {
+	z := html.NewTokenizer(body)
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document
+			return
+		case tt == html.SelfClosingTagToken:
+			t := z.Token()
+
+			isComicImg := t.Data == "img" && checkTagAttr(t, "class", "img-responsive img-comic")
+
+			// Hack: first img tag is the latest comic
+			if isComicImg {
+				title, err = getTagAttr(t, "alt")
+				if err != nil {
+					return
+				}
+				src, err = getTagAttr(t, "src")
+				if err != nil {
+					return
+				}
+				return
+			}
+		}
+	}
+	err = errors.New(fmt.Sprintf("failed to find img tag"))
+	return
+}
+
+func Format(body io.Reader) (Message, error) {
+	title, src, err := parseHtml(body)
+	if err != nil {
+		return Message{}, err
+	}
+
+	msg := Message{
+		Color:          "gray",
+		Message:        "<span>" + title + "</span><br><img src='" + src + "'/>",
+		Message_format: "html",
+		Notify:         true,
+	}
+
+	return msg, nil
 }
--- net/urlcmds.go
+++ net/urlcmds.go
@@ -1,22 +1,43 @@
 package net
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"github/chr1sto14/dilbert/formathipchat"
+	"io"
 	"net/http"
 )
 
-func FetchUrl(url string) ([]byte, error) {
+func PostMsg(url string, msg formathipchat.Message) error {
 
+	jsonStr, err := json.Marshal(msg)
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to marshal msg"))
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(jsonStr)))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to create request"))
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to post to %s", url))
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func FetchUrl(url string) (io.Reader, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("failed to fetch %s", url))
 	}
 
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("failed to parse %s", url))
-	}
-	return bytes, nil
+	return resp.Body, nil
 }
